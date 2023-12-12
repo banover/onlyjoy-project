732f16c91e0ee05ddd4013cb78621905a5137bbe
banover <qkrwotn5713@naver.com>
2023-12-12 16:18:22 +0900

    hide api keys

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const FOOTBALL_API_TOKEN = "1c3b561f4bea453b9b5ee70e5b097924";
+export const FOOTBALL_API_TOKEN = "";
 export const BASE_URL = "/api"; // proxy 활용, vite.config.js 파일 참고
-export const YOUTUBE_API_KEY = "AIzaSyDWm_f_tw3NmHBDZ11cbdf2xpOfLLogvXo";
-//9일 기준, api key 새로 바꿈
+export const YOUTUBE_API_KEY = "";
+//12일 기준, api key 새로 바꿈, gitignore 활용하기..
 export const ONE_HOURS = 3600000;
 export const NUMBER_OF_SPINNING_LOGO = 3;
