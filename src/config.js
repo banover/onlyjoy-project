3e63352df2ad6799bd3420f597a8367606506765
banover <qkrwotn5713@naver.com>
2023-12-13 11:09:59 +0900

    remove git cached

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const FOOTBALL_API_TOKEN = "1c3b561f4bea453b9b5ee70e5b097924";
-export const BASE_URL = "/api"; // proxy 활용, vite.config.js 파일 참고
-export const YOUTUBE_API_KEY = "AIzaSyCS9oNcVr9hy6xhJ6Rq0gq64CxUqQpkiJU";
-//12일 기준, api key 새로 바꿈, gitignore 활용하기..
-export const ONE_HOURS = 3600000;
-export const NUMBER_OF_SPINNING_LOGO = 3;
