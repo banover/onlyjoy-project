c6c6fcb3df9e16db3e33c08c2a754c367e9fb65d
banover <qkrwotn5713@naver.com>
2024-01-12 17:03:25 +0900

    DateString.test.js 구현

--- tests/DateString.test.js
+++ tests/DateString.test.js
@@ -1,14 +1,21 @@
-import { describe, it, expect, test } from "vitest";
+import { describe, it, expect, test, beforeEach, vi } from "vitest";
 import DateString from "../src/helpers/DateString";
 
 describe("DateString class", () => {
-  it("어제를 기준으로 일주일 후의 날짜 반환 ", () => {
+  beforeEach(() => {
+    const date = new Date(2023, 0, 12);
+    vi.useFakeTimers();
+    vi.setSystemTime(date);
+  });
+
+  it("어제를 기준으로 일주일 후의 날짜 반환", () => {
     // AAA 참고
     const date = new DateString();
 
-    const dateFromYesterdayToOneWeek = date.afterAWeekFromYesterday;
+    const dateFromYesterdayToOneWeekAfter = date.afterAWeekFromYesterday;
 
-    expect(dateFromYesterdayToOneWeek).toBe("2024-01-05");
+    expect(dateFromYesterdayToOneWeekAfter).toBe("2023-01-18");
+    vi.useRealTimers();
   });
 
   test("어제 날짜 반환", () => {
@@ -16,10 +23,7 @@ describe("DateString class", () => {
 
     const yesterdayDate = date.yesterday;
 
-    expect(yesterdayDate).toBe("2023-12-29");
+    expect(yesterdayDate).toBe("2023-01-11");
+    vi.useRealTimers();
   });
 });
-
-// coverage 분석하는 법 확인하기
-// toBe 부분 좀더 꼼꼼히 알아서 string 예상 값 만들게
-// branch나눠서 test부분!
